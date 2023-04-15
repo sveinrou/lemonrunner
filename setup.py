007fad5d739fab3ac22b784909a557004fd72bb5
sveinrou <svein.rougseth@norbit.com>
2023-04-15 15:29:50 +0200

    No requirements

--- setup.py
+++ setup.py
@@ -3,8 +3,6 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-requirements = []
-
 if __name__ == '__main__':
     setuptools.setup(
         name="lemonrunner",
@@ -16,7 +14,6 @@ if __name__ == '__main__':
         long_description_content_type="text/markdown",
         url="https://github.com/sveinrou/lemonrunner",
         packages=setuptools.find_packages(),
-        install_requires=requirements,
         classifiers=[
             "Programming Language :: Python :: 3",
             "Operating System :: OS Independent",
